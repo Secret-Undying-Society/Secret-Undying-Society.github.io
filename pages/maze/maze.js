c0134ff5222205b50d329ded585032800c08b1eb
phbui <101759075+phbui@users.noreply.github.com>
2023-04-21 16:19:41 -0400

    added maze js

---
+++ pages/maze/maze.js
@@ -0,0 +1,18 @@
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const title = document.getElementById("title");
+
+const directions = ["up", "down", "left", "right", "in", "out", "live", "die", "cry", "run"];
+const encouragement = [    "don't give up now",    "hang in there",    "stay the course",    "keep pushing forward",    "you're getting closer",    "keep your eye on the prize",    "one step at a time",    "keep moving forward",    "stay focused and determined",    "the finish line is in sight",    "keep the momentum going",    "don't stop now",    "keep on truckin'",    "keep fighting the good fight",    "you're making progress",    "keep your head up",    "don't look back, keep moving forward",    "the end is in sight",    "just a little further",    "you're on the right track"];
+
+
+function randomize() {
+    left.innerText = directions[Math.floor(Math.random() * directions.length)];
+    right.innerText = directions[Math.floor(Math.random() * directions.length)];
+    title.innerHTML = encouragement[Math.floor(Math.random() * encouragement.length)];
+    let test = Math.floor(Math.random() * 5); 
+    console.log (test);
+    if (test === 4) {
+        window.location.href = "right.html";
+    }
+}
